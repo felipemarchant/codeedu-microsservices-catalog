78c760961bc86b4d7108f3cca0e4a3141862d2ca
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2020-11-28 09:18:20 -0300

    Test create Genre

---
+++ tests/Feature/Models/GenreTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Genre;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class GenreTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Teste listagem de categorias
+     * @return void
+     */
+    public function testList()
+    {
+        factory(Genre::class, 1)->create();
+        $genres = Genre::all();
+        $this->assertCount(1, $genres);
+        $genreKey = array_keys($genres->first()->getAttributes());
+        $this->assertEqualsCanonicalizing([
+            'id',
+            'name',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ], $genreKey);
+    }
+}
