153ca0dfd89e1142223bf96990bd3d698d75e5d1
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2021-01-20 09:41:20 -0300

    Refatoraçao test

--- tests/Feature/Http/Controllers/Api/GenreControllerTest.php
+++ tests/Feature/Http/Controllers/Api/GenreControllerTest.php
@@ -58,19 +58,12 @@ class GenreControllerTest extends TestCase
      */
     public function testUpdate()
     {
-        $genre = factory(Genre::class)->create(['is_active' => false]);
-        $response = $this->json('PUT', route('genres.update', ['genre' => $genre->id]), [
-            'name' => 'test',
-            'is_active' => true,
-        ]);
-        $id = $response->json('id');
-        $genre = Genre::find($id);
-        $response->assertStatus(200)
-                 ->assertJson($genre->toArray())
-                 ->assertJsonFragment(['is_active' => true]);
-        $response = $this->json('PUT', route('genres.update', ['genre' => $genre->id]), [
-            'name' => 'test',
-        ]);
+        $this->genre = factory(Genre::class)->create();
+        $data = ['name' => 'test'];
+        $response = $this->assertUpdate($data, $data + ['deleted_at' => null, 'is_active' => false]);
+        $response->assertJsonStructure(['created_at', 'updated_at']);
+        $data = ['name' => 'test', 'is_active' => true];
+        $this->assertUpdate($data, $data + array_merge($data, ['deleted_at' => null, 'is_active' => true]));
     }
 
     public function testDestroy()
